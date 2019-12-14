b91c1978b5143064ac99cc9491a82d03135cfa86
Carlos Rios Rios <carios@MacBook-Air.local>
2019-12-14 17:54:33 -0600

    Cambios menores

--- SeleniumFirts/src/newproject/myclass.java
+++ SeleniumFirts/src/newproject/myclass.java
@@ -1,5 +1,8 @@
 package newproject;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,6 +23,10 @@ public class myclass {
        
         System.out.println(alertMessage);
         driver.quit();
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+//        Path currentDir = Paths.get(".");
+//        System.out.println(currentDir.toAbsolutePath().getParent().getParent() + "/Libraries");
        
     }
 
